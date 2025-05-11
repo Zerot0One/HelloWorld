7fb1f202dc3c08621984bfda40ad6ac24546d9b2
小王 <123@qq.com>
2025-05-11 21:33:05 +0800

    实现sort功能

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-void sort(int arr[], int size);
+void sort(int arr[], int size) {
+    int tmp = 0;
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - 1 - i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+
+}
 int main() {
     int arr[] = {10, 3, 5, 8, 9, 14, 1};
     int size = sizeof (arr) / sizeof(arr[0]);
